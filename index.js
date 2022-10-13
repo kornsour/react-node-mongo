a5a0e3acd6bb60b95fba814f302d5dda15ca672e
kornsour <ajkaiserauer@gmail.com>
2022-10-13 14:09:08 -0400

    google oauth

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 require("dotenv").config();
 
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ hi: "there" });
-});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.googleClientID,
+      clientSecret: process.env.googleClientSecret,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken) => {
+      console.log(accessToken);
+    }
+  )
+);
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
+);
+
+app.get("auth/google/callback", passport.authenticate("google"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
